6037cdb4ff4e355aa7d460f591bedda20a92e032
Satish Tiwari <satish.tiwari@nagarro.com>
2024-10-03 16:20:31 +0530

    my first commit

---
+++ src/main/java/com/example/account/model/Account.java
@@ -0,0 +1,26 @@
+package com.example.account.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="accounts")
+@ToString
+public class Account {
+    @Id
+    private Long accountNumber;
+    @Column(nullable = false, length = 100)
+    private String accountName;
+    private String userEmail;
+    private  Double balance;
+
+}
